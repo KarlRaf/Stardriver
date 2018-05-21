e057f55b7f6cccaecf20be5ad1bdfdc853a65808
Pierre-Etienne Soury <pierretienne69@gmail.com>
2018-05-21 17:21:32 +0200

    seed in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, bio: Faker::BackToTheFuture.quote)
+end
