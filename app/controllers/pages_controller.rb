3da7cce1af6a728a42e3b2d90723612a9c787e8d
Nathan Thedrez <nathan.thedrez@gmail.com>
2018-05-21 18:03:46 +0200

    search bar not in function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+
+  end
+
+  def index
+    @flights = Flight.all
+  end
+
+  def show
+    @flight = Flight.find(params[:destination])
   end
 end
