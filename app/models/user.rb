073b94d1bbb2bfdfd801829520bdec447afee6bd
Pierre-Etienne Soury <pierretienne69@gmail.com>
2018-05-22 12:09:07 +0200

    added the picture option to the signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
+  mount_uploader :photo, PhotoUploader
+
 end
