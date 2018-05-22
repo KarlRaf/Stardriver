d9b8ab5830c08128b4e2f83ddb6f1b6fdb2ae0f0
Karl Raf <karlforbusiness@gmail.com>
2018-05-22 16:01:57 +0200

    photo uploader rockets  and display for user and rockets

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -2,5 +2,5 @@ class Rocket < ApplicationRecord
   belongs_to :user
   has_many :flights, dependent: :destroy
   validates :capacity, :model, presence: true
-
+  mount_uploader :photo, PhotoUploader
 end
