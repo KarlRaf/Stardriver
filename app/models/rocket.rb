f9ae8941bcfaed3b60a0636a8be6891da90e863d
Pierre-Etienne Soury <pierretienne69@gmail.com>
2018-05-22 15:45:00 +0200

    modifications added

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -3,4 +3,9 @@ class Rocket < ApplicationRecord
   has_many :flights, dependent: :destroy
   validates :capacity, :model, presence: true
 
+  def diplay
+    str = "A model #{self.model} with #{self.capacity} sits."
+    return str
+  end
+
 end
