a7b4ec4aaca7f516eda0db9b31801bc51905f039
Nathan Thedrez <nathan.thedrez@gmail.com>
2018-05-24 15:21:17 +0200

    maj card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-
   def home
+    @flights = Flight.all.first(6)
+    #flights = @flights.sort_by{|flight| flight.destination}
+    #@flights_by_dest = [flights.first]
+    #flights.each.with_index do |flight, index|
+    #  @flights_by_dest << flight if @flights_by_dest.last.destination != flight.destination
+    #end
   end
 end
+
+
