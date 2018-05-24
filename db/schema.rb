2a03ee0f3db144ed24d1e8b09275924f844e1153
Karl Raf <karlforbusiness@gmail.com>
2018-05-24 11:10:02 +0200

    register navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_134041) do
+ActiveRecord::Schema.define(version: 2018_05_23_100328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_05_22_134041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "name"
     t.index ["user_id"], name: "index_rockets_on_user_id"
   end
 
